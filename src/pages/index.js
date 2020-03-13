141a25e2d3a1b2dde2db3a848f301a1d151145fa
Walter Marinho <waltersmarinho@edu.unifil.br>
2020-03-13 18:56:04 -0300

    Cleanup homepage links

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,6 @@ const IndexPage = () => {
         I'm Walter, I'm a Computer science student from Londrina and I'm
         currently studying Gatsby for a college project.
       </h2>
-      <p>
-        Want to talk, or have a job offer?{" "}
-        <Link to="/contact">Contact me.</Link>
-      </p>
     </Layout>
   )
 }
